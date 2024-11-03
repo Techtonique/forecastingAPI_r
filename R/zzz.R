05b89583212f4ece88cf90335f76c7cf782ce821
Thierry Moudiki <thierry.moudiki@gmail.com>
2024-11-03 10:22:40 +0100

    change BASE_URL

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,6 @@
 .onLoad <- function(libname, pkgname) {
-    .BASE_URL <<- "http://127.0.0.1:8000"
+    #.BASE_URL <<- "http://127.0.0.1:8000"
+    .BASE_URL <<- "https://www.techtonique.net"
 }
 
 # In R/another_file.R
